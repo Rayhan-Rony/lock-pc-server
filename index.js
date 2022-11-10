9c7b9231ddb0ad3fa8156b1e8144f1f1e53f02b1
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-11-10 15:50:03 +0600

    added dotenv file

--- index.js
+++ index.js
@@ -3,20 +3,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
+// middleWare 
 app.use(cors())
 app.use(express.json())
-// lockpc
-// qX74ZUQR7c6bRjCu
 
-
-// ******* dotenv 
-
-// app.get('/', (req, res) => {
-//     res.send('Server running')
-// })
-
-const uri = "mongodb+srv://lockpc:qX74ZUQR7c6bRjCu@cluster0.t1ebet1.mongodb.net/?retryWrites=true&w=majority";
+// Mongobd Info 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t1ebet1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -25,9 +19,12 @@ async function run() {
         const servicesCollection = client.db('lockpc').collection('services')
         const reviewsCollection = client.db('lockpc').collection('reviews')
 
+
+        // api 
+
         app.get('/', async (req, res) => {
             const query = {};
-            const cursor = servicesCollection.find(query).limit(3);
+            const cursor = servicesCollection.find(query).sort({ datefield: -1 }).limit(3);
             const services = await cursor.toArray()
             res.send(services)
         })
@@ -39,7 +36,6 @@ async function run() {
             res.send(services)
         })
         app.get('/services/:id', async (req, res) => {
-            // console.log(req.params.id)
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const service = await servicesCollection.findOne(query)
@@ -66,7 +62,6 @@ async function run() {
         app.get('/reviews/:id', async (req, res) => {
             const id = req.params.id
             const query = { service_id: parseInt(id) }
-            // console.log(query)
             const cursor = reviewsCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
@@ -74,16 +69,13 @@ async function run() {
 
         app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id
-            console.log(id)
             const query = { _id: ObjectId(id) }
             const result = await reviewsCollection.deleteOne(query)
             res.send(result)
         })
         app.patch('/reviews/:id', async (req, res) => {
             const id = req.params.id;
-            // console.log(id)
             const message = req.body.message;
-            // console.log(message);
             const query = { _id: ObjectId(id) }
             const updatedMessage = {
                 $set: {
@@ -91,6 +83,11 @@ async function run() {
                 }
             }
             const result = await reviewsCollection.updateOne(query, updatedMessage)
+            res.send(result)
+        })
+        app.post('/services', async (req, res) => {
+            const service = req.body
+            const result = await servicesCollection.insertOne(service)
             res.send(result)
         })
 
